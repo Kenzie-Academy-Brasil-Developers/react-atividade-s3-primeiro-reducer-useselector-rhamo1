df816a5826c79e12e1e9a57792aafdd18eaa4f8a
rhamo1 <rhamo1@outlook.com>
2021-11-22 17:16:30 -0300

    feat: add fruits.

--- src/pages/Fruits/index.jsx
+++ src/pages/Fruits/index.jsx
@@ -1,20 +1,34 @@
 //importando useSelector do react-redux
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import "./styles.css";
 import useSound from "use-sound";
+import { useState } from "react";
 
 import banana from "./banana.mp3";
 import morango from "./morango.mp3";
+import { addFruit } from "../../store/modules/fruits/action";
 
 const FruitsPage = () => {
-  //acessando o estado global e pegando o state fruits
+  const [fruit, setFruit] = useState("");
+  const dispatch = useDispatch();
   const fruits = useSelector((state) => state.fruits);
+
+  const handleAddFruit = () => dispatch(addFruit(fruit));
 
   const [audio] = useSound(banana, { volume: 0.25 });
   const [audio2] = useSound(morango, { volume: 0.25 });
 
   return (
     <div className="container">
+      <input
+        className="box"
+        type="text"
+        onChange={(e) => setFruit(e.target.value)}
+      />
+      <button className="box" onClick={handleAddFruit}>
+        Adicionar frutas
+      </button>
+
       {fruits.map((fruit) => (
         <h2 onMouseDown={audio} onMouseUp={audio2} key={fruit}>
           {" "}
--- src/store/modules/fruits/reducer.js
+++ src/store/modules/fruits/reducer.js
@@ -1,6 +1,11 @@
 const initialState = ["Banana", "Maçã", "Morango"];
 const fruitsReducer = (state = initialState, action) => {
+  console.log(action);
   switch (action.type) {
+    case "ADD_FRUIT":
+      const { fruit } = action;
+      return [...state, fruit];
+
     default:
       return state;
   }
